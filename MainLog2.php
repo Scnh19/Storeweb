75a32a2afa06ed219c56bb1493c41bb0846e1773
Jan Repalda <zjrcrepalda@917ventures.com>
2024-06-12 17:54:54 +0800

    working MainLog2.php

--- MainLog2.php
+++ MainLog2.php
@@ -13,40 +13,30 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
 	$email = validate($_POST['email']);
 	$pass = validate($_POST['password']);
 
-	if (empty($email)) {
-		header("Location: index.php?error=email is required");
+	if (empty($email) || empty($pass) ) {
+		header("Location: index.php?error=email and password is required");
 	    exit();
-	}else if(empty($pass)){
-        header("Location: index.php?error=password is required");
-	    exit();
-	}else{
-		$sql = "SELECT * FROM users WHERE email='$email' AND password='$pass'";
+	}
+	else{
+		$sql = "SELECT * FROM users WHERE email='$email'";
 		$result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
 		$count = mysqli_num_rows($result);
 		$row = mysqli_fetch_assoc($result);
-		$hashword =  $row['password'];
-
-		if (password_verify($_POST['password'],$hashword)) {
-            if ($row['email'] === $email && $row['password'] === $pass) {
-				$_SESSION['user_name'] = $row['user_name'];
-            	$_SESSION['email'] = $row['email'];
-            	$_SESSION['id'] = $row['id'];
-				header("Location: HomePage2.php");
-                exit();
-				
-            }else{
-				header("Location: index.php?error=Incorect email or password");
-		        exit();
+		if (mysqli_num_rows($result) === 1) {
+			if (mysqli_num_rows($result) === 1) {
+			if (password_verify($_POST['password'],$row['password'])) {
+					$_SESSION['user_name'] = $row['user_name'];
+					$_SESSION['email'] = $row['email'];
+					$_SESSION['id'] = $row['id'];
+					header("Location: HomePage2.php");
+					exit();
 			}
-		}else{
-			header("Location: index.php?error=Incorect email or password");
-	        exit();
+			else{ header("Location: index.php?error=email or password is wrong");}
+			}
 		}
+		else{ header("Location: index.php?error=email or password is wrong");}
 	}
 	
 }
-else{
-	header("Location: index.php");
-	exit();
-}
+
 ?>
